119fbe3c9aff8b5cc3c270a1ee15c3bc686dfbee
Sushanth R <sushanthkrishnan315@gmail.com>
2022-05-30 23:31:25 +0530

    Solve Roman to int

--- LC/easy/RomanToInteger.py
+++ LC/easy/RomanToInteger.py
@@ -1,12 +1,10 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman = {"I": (1, 7), "V": (5, 6), "X": (10, 5), "L": (50, 4), "C": (100, 3), "D": (500, 2), "M": (1000, 1)}
-        result = 0
-        operation = "add"
-        for i in range(len(s) - 1, -1, -1):
-            result += roman[s[i]][0] if operation == "add" else -roman[s[i]][0]
-            if i > 0:
-                operation = "add" if roman[s[i - 1]][1] <= roman[s[i]][1] else "subtract"
+        roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        length = len(s)
+        result = roman[s[length - 1]]
+        for i in range(length - 2, -1, -1):
+            result += roman[s[i]] if roman[s[i + 1]] <= roman[s[i]] else -roman[s[i]]
         return result
 
 
